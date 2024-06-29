60187b908d09ce5a7d2193d1a9096dc5a8a539f9
ayush-git-hub <159812035+ayush-git-hub@users.noreply.github.com>
2024-06-29 21:39:47 +0530

    update

--- src/pages/Userpostpage.jsx
+++ src/pages/Userpostpage.jsx
@@ -4,6 +4,27 @@ import { useNavigate, useParams } from 'react-router-dom';
 import Postcard from '../components/Postcard';
 import Toast from '../components/Toast';
 
+import image1 from '../assets/1.png';
+import image2 from '../assets/2.png';
+import image3 from '../assets/3.png';
+import image4 from '../assets/4.png';
+import image5 from '../assets/5.png';
+import image6 from '../assets/6.png';
+import image7 from '../assets/7.png';
+import image8 from '../assets/8.png';
+import image9 from '../assets/9.png';
+import image10 from '../assets/10.png';
+import image11 from '../assets/11.png';
+import image12 from '../assets/12.png';
+import image13 from '../assets/13.png';
+import image14 from '../assets/14.png';
+import image15 from '../assets/15.png';
+import image16 from '../assets/16.png';
+import image17 from '../assets/17.png';
+import image18 from '../assets/18.png';
+import image19 from '../assets/19.png';
+import image20 from '../assets/20.png';
+
 const Userpostpage = () => {
   const [posts, setPosts] = useState([]);
   const [userDetail, setUserDetail] = useState({});
